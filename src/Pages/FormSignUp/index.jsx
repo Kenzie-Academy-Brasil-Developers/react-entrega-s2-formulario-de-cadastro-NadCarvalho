1b9df3223b56b32fd4a94a32e9fe94696e29c823
NadCarvalho <carvalho.nadiacaroline@gmail.com>
2021-09-21 15:37:33 -0300

    criando conteudo de formSignUp e WelcomePage

---
+++ src/Pages/FormSignUp/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Form from '../../Components/Form'
+import Button from '../../Components/Button'
+
+const FormSignUp = () => {
+  return(
+<div>
+  <Form/>
+  <Button>Cadastrar</Button>
+</div>
+  )
+}
+
+export default FormSignUp;
---
+++ src/Pages/WelcomePage/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import WelcomeMessage from '../../Components/WelcomeMessage'
+import Button from '../../Components/Button'
+
+
+const WelcomePage = () => {
+  return(
+<div>
+  <WelcomeMessage/>
+  <Button>Voltar</Button>
+</div>
+  )
+}
+
+export default WelcomePage;
